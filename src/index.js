2bb080dbcb3c634b655048ce9a0a1ab41c8032e6
Chris Wallace <chris@g33kchris.net>
2017-11-02 00:25:27 +0000

    Updated statestrap to support redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const createStore = require('redux').createStore;
 const Provider = require('react-redux').Provider;
 
 const stateStrap = (state, Container) => {
-  const bootstrappedStore = createStore(() => state);
+  const bootstrappedStore = createStore(
+    () => state,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   return (
     <Provider store={bootstrappedStore}>
       <Container />
