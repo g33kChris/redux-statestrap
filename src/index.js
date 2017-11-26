bd8dbca33bfe8871abd54b078b986b35b8900af3
Chris Wallace <chris@g33kchris.net>
2017-11-26 12:16:56 +0000

    Added extra param to compose enhancers

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const React = require('react');
 const createStore = require('redux').createStore;
 const Provider = require('react-redux').Provider;
 
-const stateStrap = (state, Container) => {
-  const bootstrappedStore = createStore(() => state);
+const stateStrap = (state, Container, preloadedState = {}, composeEnhancers) => {
+  const bootstrappedStore = createStore(() => state, preloadedState, (composeEnhancers && composeEnhancers()));
   return (
     <Provider store={bootstrappedStore}>
       <Container />
